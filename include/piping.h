e1a47f7e085a2244fef48665f65a23421063c254
hugh <hal2209122@gmail.com>
2025-09-23 22:55:08 -0400

    move piping to own func

--- include/piping.h
+++ include/piping.h
@@ -1,10 +1,14 @@
 #ifndef PIPING_H
 #define PIPING_H
 
+#include "lexer.h"
+
 void cleanProcesses(int* processes , int processCount , int** pipes , int pipesCount);
 
 void createChildProcesses(char** commands , int numCommands , char*** args);
 
 int** createPipes( int numProcesses);
+
+void pipeCommands(tokenlist* tokens);
 
 #endif
